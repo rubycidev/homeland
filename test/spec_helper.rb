ff41b5f7e375259f09a6b62e1b0c31561d863cff
rubycidev <117899216+rubycidev@users.noreply.github.com>
2024-01-20 21:47:14 +0100

    Update spec_helper.rb remove fast rubyci code

--- test/spec_helper.rb
+++ test/spec_helper.rb
@@ -3,22 +3,7 @@
 require_relative "test_helper"
 require "minitest/spec"
 require "minitest/autorun"
-if ENV["RUBY_CI_SECRET_KEY"]
-  require "rspec/core/runner"
-  require "ruby_ci/runner_prepend"
 
-  class RSpec::Core::ExampleGroup
-    def self.filtered_examples
-      rubyci_scoped_ids = Thread.current[:rubyci_scoped_ids] || ""
-
-      RSpec.world.filtered_examples[self].filter do |ex|
-        rubyci_scoped_ids == "" || /^#{rubyci_scoped_ids}($|:)/.match?(ex.metadata[:scoped_id])
-      end
-    end
-  end
-
-  RSpec::Core::Runner.prepend(RubyCI::RunnerPrepend)
-end
 Minitest::Spec.register_spec_type(/Controller$/, ActionDispatch::IntegrationTest)
 
 require_relative "support/api_controller_test"
